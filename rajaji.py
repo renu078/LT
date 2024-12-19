44258dd79863b476d6fb406d7e7b3c17cc61b832
renu078 <rt2436488@gmail.com>
2024-12-19 16:27:32 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7870345592:AAGX8dEXm3bjZcwJeGoK59mq8tuBGHqUdWI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Shailesh2346")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1549147628")
+
+
+
+
+  
